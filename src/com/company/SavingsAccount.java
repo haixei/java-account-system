ca1be49818b14678f0f6f83e0343a85be585236c
haixei <aleksandraosuch002@gmail.com>
2021-06-07 12:26:15 +0200

    Small fixes, deleting not used files

--- src/com/company/SavingsAccount.java
+++ src/com/company/SavingsAccount.java
@@ -1,12 +1,15 @@
 package com.company;
 
 import java.math.BigDecimal;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class SavingsAccount implements Account{
     private BigDecimal balance;
-    private final double percent = 1.5;
+    private final BigDecimal percent;
     private String dateCreated;
     String accountNumber;
     String clientId;
@@ -15,7 +18,27 @@ public class SavingsAccount implements Account{
         IdGenerator generator = new IdGenerator();
         this.dateCreated = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
         this.clientId = clientId;
+        this.percent = new BigDecimal("1.5");
         this.accountNumber = generator.generateAccountId("SA");
+    }
+
+    public BigDecimal calculateMoneySaved(){
+        // Calculated the % saved from the input date
+        BigDecimal decMultiplied = this.balance.multiply(this.percent);
+        BigDecimal savedPerMonth = decMultiplied.divide(new BigDecimal("100"));
+
+        // Calculate amount of months from opening the account
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String startDate = sdf.format(this.dateCreated);
+
+        // Get the current date
+        String endDate = sdf.format(new Date());
+
+        long monthsBetweenLong = ChronoUnit.MONTHS.between(
+                             LocalDate.parse(startDate).withDayOfMonth(1),
+                             LocalDate.parse(endDate).withDayOfMonth(1));
+
+        return savedPerMonth.multiply(new BigDecimal(monthsBetweenLong));
     }
 
     @Override
--- src/com/company/Transaction.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class Transaction {
-    public float recipientAccountNumber;
-    public float transactionAmount;
-    private final long transactionDate = System.currentTimeMillis();
-}
